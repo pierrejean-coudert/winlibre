1520fba2e73b86673d896edd1ad1aa4fa896d7e5
Alejandro Cadavid <acadavid@gmail.com>
2009-07-06 15:53:52 -0500

    Redirection complete. Changed installed_size to selfalled_size. Cleaning some code on handlers.py

--- repository/api/handlers.py
+++ repository/api/handlers.py
@@ -9,60 +9,34 @@ import sys
 sys.path.append('..')
 from repository.winrepo.models import *
 
+### Default fields list
+defaultfields =  ('name', 'version', 'architecture', 'filename', 'short_description', 'long_description', 'creator', 'creator_email', 'publisher', 'publisher_email', 'maintainer', 'maintainer_email', 'rights_holder', 'rights_holder_email', 'release_date', 'changes', 'size', 'license', 'sha256', 'homepage', 'selfalled_size', 
+('languages', ('language', ), ),
+('section', ('title', ), ),
+('supported', ('os_version', ), ),
+('depends', ('name', 'version', ), ), 
+('provides', ('name', 'version', ), ), 
+('suggests', ('name', 'version', ), ),  
+('replaces', ('name', 'version', ), ),
+('pre_depends', ('name', 'version', ), ),
+('recommends', ('name', 'version', ), ),
+('conflicts', ('name', 'version', ), )
+)
+
+
 class AnonymousPackageHandler(AnonymousBaseHandler):
     model = Package
-    fields = ('name', 'version', 'architecture', 'filename', 'short_description', 'long_description', 'creator', 'creator_email', 'publisher', 'publisher_email', 'maintainer', 'maintainer_email', 'rights_holder', 'rights_holder_email', 'release_date', 'changes', 'size', 'license', 'sha256', 'homepage', 'installed_size', 
-('languages', ('language', ), ),
-('section', ('title', ), ),
-('supported', ('os_version', ), ),
-('depends', ('name', 'version', ), ), 
-('provides', ('name', 'version', ), ), 
-('suggests', ('name', 'version', ), ),  
-('replaces', ('name', 'version', ), ),
-('pre_depends', ('name', 'version', ), ),
-('recommends', ('name', 'version', ), ),
-('conflicts', ('name', 'version', ), )
-# Languages and supported are not yet implemented
-)
-#   allow_methods = {'GET',}
-
+    fields = defaultfields
 
 class PackageHandler(BaseHandler):
     anonymous = AnonymousPackageHandler
     model = Package
-    fields = ('name', 'version', 'architecture', 'filename', 'short_description', 'long_description', 'creator', 'creator_email', 'publisher', 'publisher_email', 'maintainer', 'maintainer_email', 'rights_holder', 'rights_holder_email', 'release_date', 'changes', 'size', 'license', 'sha256', 'homepage', 'installed_size', 
-('languages', ('language', ), ),
-('section', ('title', ), ),
-('supported', ('os_version', ), ),
-('depends', ('name', 'version', ), ), 
-('provides', ('name', 'version', ), ), 
-('suggests', ('name', 'version', ), ),  
-('replaces', ('name', 'version', ), ),
-('pre_depends', ('name', 'version', ), ),
-('recommends', ('name', 'version', ), ),
-('conflicts', ('name', 'version', ), )
-# Languages and supported are not yet implemented
-)
-#   allow_methods = {'GET',}
-
+    fields = defaultfields
 
 class PackageHandler(BaseHandler):
     anonymous = AnonymousPackageHandler
     model = Package
-    fields = ('name', 'version', 'architecture', 'filename', 'short_description', 'long_description', 'creator', 'creator_email', 'publisher', 'publisher_email', 'maintainer', 'maintainer_email', 'rights_holder', 'rights_holder_email', 'release_date', 'changes', 'size', 'license', 'sha256', 'homepage', 'installed_size',  
-('languages', ('language', ), ),
-('section', ('title', ), ),
-('supported', ('os_version', ), ),
-('depends', ('name', 'version', ), ), 
-('provides', ('name', 'version', ), ), 
-('suggests', ('name', 'version', ), ),  
-('replaces', ('name', 'version', ), ),
-('pre_depends', ('name', 'version', ), ),
-('recommends', ('name', 'version', ), ),
-('conflicts', ('name', 'version', ), )
-# Languages and supported are not yet implemented
-)
-#   allow_methods = {'GET',}
+    fields = defaultfields
 
     def read(self, request, name=None, version=None, id=None, generalquery=None):
         base = Package.objects
@@ -72,42 +46,20 @@ class PackageHandler(BaseHandler):
                 return base.get(name=name, version=version)
             except:
                 return None #This should be modified by a default error message
+        elif name:
+            try:
+                return base.filter(name=name)
+            except:
+                return None
 
 class AnonymousPackagesHandler(AnonymousBaseHandler):
     model = Package
-    fields = ('name', 'version', 'architecture', 'filename', 'short_description', 'long_description', 'creator', 'creator_email', 'publisher', 'publisher_email', 'maintainer', 'maintainer_email', 'rights_holder', 'rights_holder_email', 'release_date', 'changes', 'size', 'license', 'sha256', 'homepage', 'installed_size',  
-('languages', ('language', ), ),
-('section', ('title', ), ),
-('supported', ('os_version', ), ),
-('depends', ('name', 'version', ), ), 
-('provides', ('name', 'version', ), ), 
-('suggests', ('name', 'version', ), ),  
-('replaces', ('name', 'version', ), ),
-('pre_depends', ('name', 'version', ), ),
-('recommends', ('name', 'version', ), ),
-('conflicts', ('name', 'version', ), )
-# Languages and supported are not yet implemented
-)
-#   allow_methods = {'GET',}
-
+    fields = defaultfields
 
 class PackagesHandler(BaseHandler):
     model = Package
     anonymous = AnonymousPackagesHandler
-    fields = ('name', 'version', 'architecture', 'filename', 'short_description', 'long_description', 'creator', 'creator_email', 'publisher', 'publisher_email', 'maintainer', 'maintainer_email', 'rights_holder', 'rights_holder_email', 'release_date', 'changes', 'size', 'license', 'sha256', 'homepage', 'installed_size',  
-('languages', ('language', ), ),
-('section', ('title', ), ),
-('supported', ('os_version', ), ),
-('depends', ('name', 'version', ), ), 
-('provides', ('name', 'version', ), ), 
-('suggests', ('name', 'version', ), ),  
-('replaces', ('name', 'version', ), ),
-('pre_depends', ('name', 'version', ), ),
-('recommends', ('name', 'version', ), ),
-('conflicts', ('name', 'version', ), )
-# Languages and supported are not yet implemented
-)
-#   allow_methods = {'GET',}
+    fields = defaultfields
     
     def read(self, request, date=None):
         base = Package.objects
