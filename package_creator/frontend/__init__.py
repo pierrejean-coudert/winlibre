154f5391f36d3b01d62b2b7134924ca6fbb6fc14
excid3 <excid3@localhost>
2009-07-16 01:01:38 -0500

    Added columns and toolbook to the wx frontend

--- package_creator/frontend/__init__.py
+++ package_creator/frontend/__init__.py
@@ -10,9 +10,10 @@ ICONS_32 = 'frontend/tango-icon-theme/32x32/'
 
 class CreatorApp(wx.App):
     def OnInit(self):
-        self.frame = wx.Frame(None, -1, creator.__appname__, size=(500,400))
+        self.frame = wx.Frame(None, -1, creator.__appname__, size=(700,500))
 
         self.createMenus()
+        self.createTabs()
         self.createHome()
         
         self.frame.Center()
@@ -20,7 +21,55 @@ class CreatorApp(wx.App):
         
         return True
     
+    def OnNewDir(self, evt):
+        dlg = wx.DirDialog(self.frame, "Choose a directory:")
+        if dlg.ShowModal() == wx.ID_OK:
+            self.home.folder.SetLabel(dlg.GetPath())
+        dlg.Destroy()
+
     def createHome(self):
+        sizer = wx.BoxSizer(wx.VERTICAL)
+        sizer.Add(wx.StaticLine(self.home), 0, 
+            wx.EXPAND|wx.LEFT|wx.RIGHT, 5)
+            
+        columns = wx.BoxSizer()
+        
+        # Column 1
+        column_1 = wx.BoxSizer(wx.VERTICAL)
+        txt = wx.StaticText(self.home, -1, 'New Package')
+        font = wx.Font(14, wx.SWISS, wx.NORMAL, wx.NORMAL)
+        txt.SetFont(font)
+        column_1.Add(txt, 0, wx.ALL, 5)
+
+        txt = wx.StaticText(self.home, -1, 'Package Folder:')
+        btn = wx.Button(self.home, -1, 'Browse')
+        horiz = wx.BoxSizer()
+        self.home.folder = wx.TextCtrl(self.home)
+        horiz.Add(txt, 0, wx.LEFT|wx.CENTER, 5)
+        horiz.Add(self.home.folder, 1, wx.LEFT|wx.RIGHT|wx.CENTER, 3)
+        horiz.Add(btn, 0, wx.RIGHT|wx.CENTER, 5)
+        self.Bind(wx.EVT_BUTTON, self.OnNewDir, btn)
+        
+        
+        column_1.Add(horiz,0, wx.EXPAND)
+        
+        # Column 2
+        column_2 = wx.BoxSizer(wx.VERTICAL)
+        txt = wx.StaticText(self.home, -1, 'Open Package')
+        font = wx.Font(14, wx.SWISS, wx.NORMAL, wx.NORMAL)
+        txt.SetFont(font)
+        column_2.Add(txt, 0, wx.ALL, 5)
+        
+        columns.Add(column_1, 1, wx.EXPAND)
+        columns.Add(wx.StaticLine(self.home, style=wx.LI_VERTICAL),
+            0, wx.EXPAND|wx.BOTTOM, 5)
+        columns.Add(column_2, 1, wx.EXPAND)
+        
+        sizer.Add(columns, 1, wx.EXPAND)
+
+        self.home.SetSizer(sizer)
+    
+    def createTabs(self):
         self.notebook = wx.Toolbook(self.frame, -1)
 
         # Icons for each tab
@@ -39,7 +88,7 @@ class CreatorApp(wx.App):
         # List of the panels, tab names and image indexs
         panels = [(self.home, 'Home', 0),
                   (self.files, 'Files', 1),
-                  (self.details, 'Details', 2),
+                  (self.details, 'Package Details', 2),
                   (self.scripts, 'Scripts', 3),
                   (self.submit, 'Submit', 4)]
 
@@ -48,7 +97,7 @@ class CreatorApp(wx.App):
         for item in icons:
             il.Add(wx.Image(os.path.join(ICONS_32, item)).ConvertToBitmap())
         self.notebook.AssignImageList(il)
-        
+
         # Add the pages
         for item in panels:
             self.notebook.AddPage(item[0], item[1], False, item[2])
