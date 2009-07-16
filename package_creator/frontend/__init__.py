7ff9cac45d5b704eb487b320102d6aaec74b17bf
Chris Oliver <excid3@gmail.com>
2009-07-15 20:11:39 -0500

    Clean toolbar style tabs with 32x32 px icons

--- package_creator/frontend/__init__.py
+++ package_creator/frontend/__init__.py
@@ -5,28 +5,47 @@ import os.path
 import wx
 from wx import xrc
 
-ICON_PATH = 'frontend/tango-icon-theme/16x16/'
+ICONS_16 = 'frontend/tango-icon-theme/16x16/'
+ICONS_32 = 'frontend/tango-icon-theme/32x32/'
 
 class CreatorApp(wx.App):
     def OnInit(self):
-        self.frame = wx.Frame(None, -1, creator.__appname__)
-        self.frame.Show()
+        self.frame = wx.Frame(None, -1, creator.__appname__, size=(500,400))
 
         self.createMenus()
         self.createHome()
         
+        self.frame.Center()
+        self.frame.Show()
+        
         return True
     
     def createHome(self):
-        self.notebook = wx.Notebook(self.frame)
+        self.notebook = wx.Toolbook(self.frame, -1)
+
+        il = wx.ImageList(32, 32)
+        il.Add(wx.Image(os.path.join(ICONS_32, 'actions/go-home.png')).ConvertToBitmap())
+        il.Add(wx.Image(os.path.join(ICONS_32, 'mimetypes/package-x-generic.png')).ConvertToBitmap())
+        il.Add(wx.Image(os.path.join(ICONS_32, 'categories/preferences-system.png')).ConvertToBitmap())
+        il.Add(wx.Image(os.path.join(ICONS_32, 'mimetypes/text-x-script.png')).ConvertToBitmap())
+        il.Add(wx.Image(os.path.join(ICONS_32, 'apps/system-software-update.png')).ConvertToBitmap())
+        self.notebook.AssignImageList(il)
         self.home = wx.Panel(self.notebook)
-        self.notebook.AddPage(self.home, 'Home')
+        self.notebook.AddPage(self.home, 'Home', False, 0)
+        self.files = wx.Panel(self.notebook)
+        self.notebook.AddPage(self.files, 'Files', False, 1)
+        self.details = wx.Panel(self.notebook)
+        self.notebook.AddPage(self.details, 'Details', False, 2)
+        self.scripts = wx.Panel(self.notebook)
+        self.notebook.AddPage(self.scripts, 'Scripts', False, 3)
+        self.submit = wx.Panel(self.notebook)
+        self.notebook.AddPage(self.submit, 'Submit', False, 4)
 
     def createMenus(self):
         # File menu
         filemenu = wx.Menu()
         item = wx.MenuItem(filemenu, 500, 'E&xit\tCtrl+x', 'Quit the application')
-        bmp = wx.Image(os.path.join(ICON_PATH, 'actions/system-log-out.png'),
+        bmp = wx.Image(os.path.join(ICONS_16, 'actions/system-log-out.png'),
             wx.BITMAP_TYPE_PNG).ConvertToBitmap()
         item.SetBitmap(bmp)
         filemenu.AppendItem(item)
@@ -38,7 +57,7 @@ class CreatorApp(wx.App):
         # Help menu
         helpmenu = wx.Menu()
         item = wx.MenuItem(helpmenu, 501, '&About\tCtrl+A', 'About the application')
-        bmp = wx.Image(os.path.join(ICON_PATH, 'apps/system-users.png'),
+        bmp = wx.Image(os.path.join(ICONS_16, 'apps/system-users.png'),
             wx.BITMAP_TYPE_PNG).ConvertToBitmap()
         item.SetBitmap(bmp)
         helpmenu.AppendItem(item)
