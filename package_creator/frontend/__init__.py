8b6fce5356dd8dce0e45671cbcec5dffc65db9d5
excid3 <excid3@localhost>
2009-07-16 22:48:13 -0500

    Can initialize packages with the UI now

--- package_creator/frontend/__init__.py
+++ package_creator/frontend/__init__.py
@@ -1,12 +1,23 @@
 """ wxPython Frontend """
 
 import creator
+import lib
 import os.path
 import wx
+import wx.lib.buttons  as  buttons
 from wx import xrc
 
 ICONS_16 = 'frontend/tango-icon-theme/16x16/'
 ICONS_32 = 'frontend/tango-icon-theme/32x32/'
+
+WELCOME_STR = """This application will help build WinLibre packages.
+
+Each package contains the binary files for the application as well information
+on the software such as Authors, Licenses, and package relationships.
+
+When you are ready you can create a new package or open an existing directory
+to continue working on a previously created package.
+"""
 
 class CreatorApp(wx.App):
     def OnInit(self):
@@ -21,53 +32,34 @@ class CreatorApp(wx.App):
         
         return True
     
-    def OnNewDir(self, evt):
-        dlg = wx.DirDialog(self.frame, "Choose a directory:")
-        if dlg.ShowModal() == wx.ID_OK:
-            self.home.folder.SetLabel(dlg.GetPath())
-        dlg.Destroy()
-
     def createHome(self):
+        text = wx.StaticText(self.home, -1, creator.__appname__)
+        font = wx.Font(18, wx.SWISS, wx.NORMAL, wx.BOLD)
+        text.SetFont(font)
+        welcome = wx.StaticText(self.home, -1, WELCOME_STR)
+        
         sizer = wx.BoxSizer(wx.VERTICAL)
-        sizer.Add(wx.StaticLine(self.home), 0, 
-            wx.EXPAND|wx.LEFT|wx.RIGHT, 5)
-            
-        columns = wx.BoxSizer()
+        sizer.Add(wx.StaticLine(self.home), 0, wx.EXPAND)
+        sizer.Add((0,0), 1)
+        sizer.Add(text, 0, wx.CENTER|wx.ALL, 5)
+        sizer.Add(welcome, 0, wx.CENTER|wx.ALL, 5)
         
-        # Column 1
-        column_1 = wx.BoxSizer(wx.VERTICAL)
-        txt = wx.StaticText(self.home, -1, 'New Package')
-        font = wx.Font(14, wx.SWISS, wx.NORMAL, wx.NORMAL)
-        txt.SetFont(font)
-        column_1.Add(txt, 0, wx.ALL, 5)
-
-        txt = wx.StaticText(self.home, -1, 'Package Folder:')
-        btn = wx.Button(self.home, -1, 'Browse...')
         horiz = wx.BoxSizer()
-        self.home.folder = wx.TextCtrl(self.home)
-        horiz.Add(txt, 0, wx.LEFT|wx.CENTER, 5)
-        horiz.Add(self.home.folder, 1, wx.LEFT|wx.RIGHT|wx.CENTER, 3)
-        horiz.Add(btn, 0, wx.RIGHT|wx.CENTER, 5)
-        self.Bind(wx.EVT_BUTTON, self.OnNewDir, btn)
-        
-        column_1.Add(horiz,0, wx.EXPAND)
-        
-        # Column 2
-        column_2 = wx.BoxSizer(wx.VERTICAL)
-        txt = wx.StaticText(self.home, -1, 'Open Package')
-        font = wx.Font(14, wx.SWISS, wx.NORMAL, wx.NORMAL)
-        txt.SetFont(font)
-        column_2.Add(txt, 0, wx.ALL, 5)
-        
-        columns.Add(column_1, 1, wx.EXPAND)
-        columns.Add(wx.StaticLine(self.home, style=wx.LI_VERTICAL),
-            0, wx.EXPAND|wx.BOTTOM, 5)
-        columns.Add(column_2, 1, wx.EXPAND)
-        
-        sizer.Add(columns, 1, wx.EXPAND)
+        bmp = wx.Image(os.path.join(ICONS_16, 'actions/document-new.png')).ConvertToBitmap()
+        self.home.new = buttons.GenBitmapTextButton(self.home, -1, bmp, 'New Package')
+        bmp = wx.Image(os.path.join(ICONS_16, 'actions/document-open.png')).ConvertToBitmap()
+        self.home.open = buttons.GenBitmapTextButton(self.home, -1, bmp, 'Open Package')
+        horiz.Add(self.home.new, 0, wx.RIGHT, 15)
+        horiz.Add(self.home.open, 0, wx.LEFT, 15)
+        sizer.Add(horiz, 0, wx.CENTER)
 
+        sizer.Add((0,0), 1)        
         self.home.SetSizer(sizer)
-    
+        
+        # Bindings
+        self.home.new.Bind(wx.EVT_BUTTON, self.OnNew)
+        self.home.open.Bind(wx.EVT_BUTTON, self.OnOpen)
+
     def createTabs(self):
         self.notebook = wx.Toolbook(self.frame, -1)
 
@@ -104,7 +96,24 @@ class CreatorApp(wx.App):
     def createMenus(self):
         # File menu
         filemenu = wx.Menu()
-        item = wx.MenuItem(filemenu, 500, 'E&xit\tCtrl+x', 'Quit the application')
+        
+        item = wx.MenuItem(filemenu, 502, '&New...\tCtrl+N', 'Create a new package')
+        bmp = wx.Image(os.path.join(ICONS_16, 'actions/window-new.png'),
+            wx.BITMAP_TYPE_PNG).ConvertToBitmap()
+        item.SetBitmap(bmp)
+        filemenu.AppendItem(item)
+        wx.EVT_MENU(self, item.GetId(), self.OnNew)
+        
+        item = wx.MenuItem(filemenu, 502, '&Open...\tCtrl+O', 'Open a package')
+        bmp = wx.Image(os.path.join(ICONS_16, 'actions/document-open.png'),
+            wx.BITMAP_TYPE_PNG).ConvertToBitmap()
+        item.SetBitmap(bmp)
+        filemenu.AppendItem(item)
+        wx.EVT_MENU(self, item.GetId(), self.OnOpen)
+        
+        filemenu.AppendSeparator()
+        
+        item = wx.MenuItem(filemenu, 500, 'E&xit\tCtrl+X', 'Quit the application')
         bmp = wx.Image(os.path.join(ICONS_16, 'actions/system-log-out.png'),
             wx.BITMAP_TYPE_PNG).ConvertToBitmap()
         item.SetBitmap(bmp)
@@ -116,7 +125,7 @@ class CreatorApp(wx.App):
         
         # Help menu
         helpmenu = wx.Menu()
-        item = wx.MenuItem(helpmenu, 501, '&About\tCtrl+A', 'About the application')
+        item = wx.MenuItem(helpmenu, 501, '&About...\tCtrl+A', 'About the application')
         bmp = wx.Image(os.path.join(ICONS_16, 'apps/system-users.png'),
             wx.BITMAP_TYPE_PNG).ConvertToBitmap()
         item.SetBitmap(bmp)
@@ -131,11 +140,39 @@ class CreatorApp(wx.App):
     def OnClose(self, e):
         self.frame.Close()
         
+    def OnNew(self, e):
+        path = self.GetDir()
+        os.chdir(path)
+        try:
+            lib.init(False)
+            #wx.MessageBox('Success')
+        except:
+            wx.MessageBox("An error occurred creating the project.\n" \
+            "Make sure a package does not already exist in that folder.",
+            "Error creating project")
+    
+    def OnOpen(self, e):
+        path = self.GetDir(False)
+        print path
+        os.chdir(path)
+    
     def OnAbout(self, e):
         d = wx.AboutDialogInfo()
         d.Name = creator.__appname__
         d.Version = creator.__version__
-        wx.AboutBox(d)        
+        wx.AboutBox(d)
+
+    def GetDir(self, enable_new=True):
+        """ Returns a directory """
+        if enable_new:
+            dlg = wx.DirDialog(self.frame, 'Choose a directory:')
+        else:
+            dlg = wx.DirDialog(self.frame, 'Choose a directory:',
+                style=wx.DD_DIR_MUST_EXIST)
+        if dlg.ShowModal() == wx.ID_OK:
+            result = dlg.GetPath()
+        dlg.Destroy()
+        return result or None
     
     def setLogger(self, logger):
         self.logger = logger
